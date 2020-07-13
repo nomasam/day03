0c4ddc3b0817eef11f777c8ec3422a2dea2d9892
Nomasam Mbanjwa <nombanjw@c4r16s6.wethinkcode.co.za>
2020-07-13 10:57:59 +0200

    ex00

---
+++ ex00/ft_ft.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+void pt_char(char c){
+write(1 , &c , 1);
+}
+void ft_ft(int *nbr)
+{
+int value=42;
+//nbr=& value;
+//pt_char(value);
+}
+//int main()
+//{
+//int l=42;
+//int *b=&l;
+//printf("%d" , l);
+//ft_ft(nbr);
+}
